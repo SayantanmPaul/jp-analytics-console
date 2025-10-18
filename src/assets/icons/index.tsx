5c7dae50833fb596731ba58b1c50610088bd8be9
SayantanmPaul <sayantanpaul.workspace@gmail.com>
2025-10-18 10:15:37 +0530

    feat: custom svg icons configured for LeftSidebar

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,25 @@
+import ArrowLineRight from './ArrowLineRight.svg';
+import BookOpen from './dashboards/BookOpen.svg';
+import ChartPieSlice from './dashboards/ChartPieSlice.svg';
+import FolderNotch from './dashboards/FolderNotch.svg';
+import ShoppingBagOpen from './dashboards/ShoppingBagOpen.svg';
+
+import ChatsTeardrop from './pages/ChatsTeardrop.svg';
+import IdentificationBadge from './pages/IdentificationBadge.svg';
+import IdentificationCard from './pages/IdentificationCard.svg';
+import Notebook from './pages/Notebook.svg';
+import UsersThree from './pages/UsersThree.svg';
+
+export const Icons = {
+  bookOpen: BookOpen,
+  chartPieSlice: ChartPieSlice,
+  folderNotch: FolderNotch,
+  shoppingBagOpen: ShoppingBagOpen,
+  arrowLineRight: ArrowLineRight,
+
+  chatsTeardrop: ChatsTeardrop,
+  identificationBadge: IdentificationBadge,
+  ientificationCard: IdentificationCard,
+  notebook: Notebook,
+  usersThree: UsersThree,
+};
